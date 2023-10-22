e84c8ff214caf202a52438e05338d5064d68d451
Nizar Can <nizarcan@yahoo.com>
2023-10-22 23:49:44 +0300

    refactor: split config into modules

---
+++ lua/nizarcan/remap.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
